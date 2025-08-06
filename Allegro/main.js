48cd5db0ea55bce24caea7538159b471f429059b
Janirel <113930205+Jlongarte@users.noreply.github.com>
2025-08-06 17:13:30 +0200

    main.js

--- Allegro/main.js
+++ Allegro/main.js
@@ -1,17 +1,32 @@
+/*MENU HAMBURGUESA*/
+
 const toggleBtn = document.querySelector(".menu-toggle");
 const leftMenu = document.querySelector(".left-menu");
 
 toggleBtn.addEventListener("click", () => {
   leftMenu.classList.toggle("active");
-});
 
-toggleBtn.addEventListener("click", () => {
-  leftMenu.classList.toggle("active");
-
-  // Cambiar ícono
   if (leftMenu.classList.contains("active")) {
     toggleBtn.innerHTML = "&#10005;"; // ✕
+    toggleBtn.classList.add("close-right"); // Mover a la derecha
   } else {
     toggleBtn.innerHTML = "&#9776;"; // ☰
+    toggleBtn.classList.remove("close-right"); // Volver a la izquierda
   }
 });
+
+/*BACKGROUND TRANSITION*/
+
+const heroSection = document.querySelector(".hero-section");
+const links = document.querySelectorAll(".hero-section nav a");
+
+links.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    const category = link.dataset.category;
+    heroSection.className = "hero-section " + category; // cambia clase para fondo
+  });
+  link.addEventListener("mouseout", () => {
+    heroSection.className = "hero-section default"; // vuelve a fondo por defecto
+  });
+});
+
